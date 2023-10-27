1e764f62eb74b193df3c2a7df147bc27463f3933
geovanyGonz <tigregonzo34@gmail.com>
2023-10-27 16:17:35 -0500

    Cree este proyecto de practica corto, funciona y dinamico con Javascript, bombillo de enecendido y apagado

---
+++ main.js
@@ -0,0 +1,15 @@
+const $cambiarInterruptor = document.getElementById("cambiarInterruptor");
+const $cambiarFoco = document.getElementById("cambiarFoco");
+const $switche = document.querySelector(".switch");
+
+function cambiarImagenes() {
+    if($cambiarFoco.src.match("/img/encendido-1.png") && $cambiarInterruptor.src.match("/img/on-or.png")){
+        $cambiarFoco.src = "/img/apagado-1.png";
+        $cambiarInterruptor.src = "/img/off-or.png";
+    } else {
+    $cambiarFoco.src = "/img/encendido-1.png";
+    $cambiarInterruptor.src = "/img/on-or.png"
+    }
+}
+
+$switche.addEventListener("click", cambiarImagenes)
