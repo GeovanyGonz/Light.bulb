6b4a99bc7649c3cc04c62fa8d629cef717e4ef96
geovanyGonz <tigregonzo34@gmail.com>
2023-10-28 15:43:53 -0500

    Agregue punto donde en el main.js donde hacia falta

--- main.js
+++ main.js
@@ -3,12 +3,12 @@ const $cambiarFoco = document.getElementById("cambiarFoco");
 const $switche = document.querySelector(".switch");
 
 function cambiarImagenes() {
-    if($cambiarFoco.src.match("./img/encendido-1.png") && $cambiarInterruptor.src.match("/img/on-or.png")){
+    if($cambiarFoco.src.match("./img/encendido-1.png") && $cambiarInterruptor.src.match("./img/on-or.png")){
         $cambiarFoco.src = "./img/apagado-1.png";
         $cambiarInterruptor.src = "./img/off-or.png";
     } else {
-    $cambiarFoco.src = "/img/encendido-1.png";
-    $cambiarInterruptor.src = "/img/on-or.png"
+        $cambiarFoco.src = "./img/encendido-1.png";
+        $cambiarInterruptor.src = "./img/on-or.png";
     }
 }
 
